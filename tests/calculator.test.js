b82504a424ed2d81c99c6b9366be5101222938c7
Suttirak Pimsuk <suttirak.pi@ku.th>
2025-12-25 14:59:41 +0700

    fix : toBe(5) to toBe(-5)

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,23 +1,23 @@
-const { add, subtract, multiply, divide } = require('../src/calculator');
+const { add, subtract, multiply, divide } = require("../src/calculator");
 
-describe('Calculator', () => {
-    test('adds 2 + 3 to equal 5', () => {
-        expect(add(2, 3)).toBe(5);
-    });
+describe("Calculator", () => {
+  test("adds 2 + 3 to equal 5", () => {
+    expect(add(2, 3)).toBe(5);
+  });
 
-    test('adds -2 + -3 to equal 5', () => {
-        expect(add(-2, -3)).toBe(5);
-    });
+  test("adds -2 + -3 to equal 5", () => {
+    expect(add(-2, -3)).toBe(-5);
+  });
 
-    test('subtracts 5 - 3 to equal 2', () => {
-        expect(subtract(5, 3)).toBe(2);
-    });
+  test("subtracts 5 - 3 to equal 2", () => {
+    expect(subtract(5, 3)).toBe(2);
+  });
 
-    test('multiplies 4 * 3 to equal 12', () => {
-        expect(multiply(4, 3)).toBe(12);
-    });
+  test("multiplies 4 * 3 to equal 12", () => {
+    expect(multiply(4, 3)).toBe(12);
+  });
 
-    test('divides 10 / 2 to equal 5', () => {
-        expect(divide(10, 2)).toBe(5);
-    });
+  test("divides 10 / 2 to equal 5", () => {
+    expect(divide(10, 2)).toBe(5);
+  });
 });
